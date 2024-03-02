261b3c9642ae37329bf90dd2e0d8e4a516f7bced
Manish <manish.prajapat94@gmail.com>
2024-03-02 23:19:20 +0530

    enhance coding standard and readablity of code

--- string_calculator.rb
+++ string_calculator.rb
@@ -10,31 +10,33 @@
 # Calling add with a negative number will throw an exception "negative numbers not allowed <negative_number>".
 # If there are multiple negative numbers, show all of them in the exception message, separated by commas
 class StringCalculator
-  def self.add(numbers)
-    return 0 if numbers.empty?
+  class << self
+    def add(numbers)
+      return 0 if numbers.empty?
 
-    numbers, delimiter = custom_delimer(numbers)
-    numbers = numbers.gsub(/\n/, delimiter)
-    numbers_array = numbers.split(delimiter).map(&:to_i)
+      numbers, delimiter = custom_delimer(numbers)
+      numbers = numbers.gsub(/\n/, delimiter)
+      numbers_array = numbers.split(delimiter).map(&:to_i)
 
-    check_negative_numbers(numbers_array)
+      check_negative_numbers(numbers_array)
 
-    numbers_array.sum
-  end
+      numbers_array.sum
+    end
 
-  def self.custom_delimer(numbers)
-    delimiter = ','
-    return [numbers, delimiter] unless numbers.start_with?('//')
+    def custom_delimer(numbers)
+      delimiter = ','
+      return [numbers, delimiter] unless numbers.start_with?('//')
 
-    custom_delimiter_match = %r{^//(.+)\n}.match(numbers)
-    delimiter = custom_delimiter_match[1] if custom_delimiter_match
-    numbers = numbers.sub(%r{^//.+\n}, '')
+      custom_delimiter_match = %r{^//(.+)\n}.match(numbers)
+      delimiter = custom_delimiter_match[1] if custom_delimiter_match
+      numbers = numbers.sub(%r{^//.+\n}, '')
 
-    [numbers, delimiter]
-  end
+      [numbers, delimiter]
+    end
 
-  def self.check_negative_numbers(numbers_array)
-    negatives = numbers_array.select(&:negative?)
-    raise "Negative numbers not allowed: #{negatives.join(',')}" if negatives.any?
+    def check_negative_numbers(numbers_array)
+      negatives = numbers_array.select(&:negative?)
+      raise "Negative numbers not allowed: #{negatives.join(',')}" if negatives.any?
+    end
   end
 end
